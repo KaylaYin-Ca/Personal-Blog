780cb2da773267851b17050cab74738b2e3c81bf
YIN <yin@YINdeMacBook-Air.local>
2022-05-23 22:29:46 +0800

    personal blog edition v2

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // const diaryPost = require(__dirname+"/post.js");
+const _ = require("lodash");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -20,11 +22,10 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  // res.render("home", {
-  //   homeContent: homeStartingContent,
-  //   posts:posts
-  // });
-  res.redirect("/");
+  res.render("home", {
+    homeContent: homeStartingContent,
+    posts:posts
+  });
 });
 
 app.get("/about",function(req,res){
@@ -42,6 +43,7 @@ app.get("/contact",function(req,res){
 app.get("/compose",function(req,res){
   res.render("compose");
 });
+
 app.post("/compose",function(req,res){
   // const postTitle = req.body.postTitle;
   // const postText = req.body.postText;
@@ -53,12 +55,20 @@ app.post("/compose",function(req,res){
 
   posts.push(post);
 
-  res.render("home",{
-    homeContent: homeStartingContent,
-    posts:posts
-  });
+  res.redirect("/");
 });
 
+app.get("/posts/:diary",function(req,res){
+
+  const pathParam = _.lowerCase(req.params.diary);
+  posts.forEach(function(element){
+    const diaryTitle = _.lowerCase(element.title);
+    if (pathParam === diaryTitle){
+      console.log("Match Found!");
+    }
+  });
+  // res.render("compose");
+});
 
 
 app.listen(3000, function() {
